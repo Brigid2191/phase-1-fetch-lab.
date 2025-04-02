047e9312e42f72c0fc37e2fa6c22528a8491635e
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-04-02 09:58:17 +0300

    Updated assignment

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books", { timeout: 10000 }) // 10 seconds timeout
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Failed to fetch books");
+      }
+      return resp.json();
+    })
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error("Error fetching books:", error));
 }
 
+
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('.main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
